c3030642d9ac0a5e1817659b6a94d849b108d4a1
Ping-Lin Chang <ping-lin.chang@instill.tech>
2023-02-06 16:56:03 +0000

    chore: mute healthcheck endpoints for logs

--- cmd/main/main.go
+++ cmd/main/main.go
@@ -85,7 +85,7 @@ func main() {
 		grpc_zap.WithDecider(func(fullMethodName string, err error) bool {
 			// will not log gRPC calls if it was a call to liveness or readiness and no error was raised
 			if err == nil {
-				if match, _ := regexp.MatchString("instill.connector.v1alpha.MgmtService/.*ness$", fullMethodName); match {
+				if match, _ := regexp.MatchString("vdp.mgmt.v1alpha.UserService/.*ness$", fullMethodName); match {
 					return false
 				}
 			}
