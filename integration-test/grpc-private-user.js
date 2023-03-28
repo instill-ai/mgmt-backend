688323c4647e1dab385888b46eec55cb41ed9faa
Xiaofei Du <xiaofei.du@instill.tech>
2023-03-28 01:24:10 +0100

    test: update integration tests

--- integration-test/grpc-private-user.js
+++ integration-test/grpc-private-user.js
@@ -78,8 +78,7 @@ export function CheckAdminGet() {
       'vdp.model.v1alpha.MgmtPrivateService/GetUserAdmin response id': (r) => r && r.message.user.id === constant.defaultUser.id,
       'vdp.model.v1alpha.MgmtPrivateService/GetUserAdmin response type': (r) => r && r.message.user.type === "OWNER_TYPE_USER",
       'vdp.model.v1alpha.MgmtPrivateService/GetUserAdmin response email': (r) => r && r.message.user.email !== undefined,
-      'vdp.model.v1alpha.MgmtPrivateService/GetUserAdmin response plan': (r) => r && r.message.user.plan !== undefined,
-      'vdp.model.v1alpha.MgmtPrivateService/GetUserAdmin response billingId': (r) => r && r.message.user.billingId !== undefined,
+      'vdp.model.v1alpha.MgmtPrivateService/GetUserAdmin response customerId': (r) => r && r.message.user.customerId !== undefined,
       'vdp.model.v1alpha.MgmtPrivateService/GetUserAdmin response firstName': (r) => r && r.message.user.firstName !== undefined,
       'vdp.model.v1alpha.MgmtPrivateService/GetUserAdmin response lastName': (r) => r && r.message.user.lastName !== undefined,
       'vdp.model.v1alpha.MgmtPrivateService/GetUserAdmin response orgName': (r) => r && r.message.user.orgName !== undefined,
@@ -112,7 +111,7 @@ export function CheckAdminLookUp() {
     plaintext: true
   });
 
-  // Get the uid of the default user  
+  // Get the uid of the default user
   var res = client.invoke('vdp.mgmt.v1alpha.MgmtPrivateService/GetUserAdmin', {
     name: `users/${constant.defaultUser.id}`
   })
@@ -131,8 +130,7 @@ export function CheckAdminLookUp() {
       'vdp.model.v1alpha.MgmtPrivateService/LookUpUserAdmin response id': (r) => r && r.message.user.id === constant.defaultUser.id,
       'vdp.model.v1alpha.MgmtPrivateService/LookUpUserAdmin response type': (r) => r && r.message.user.type === "OWNER_TYPE_USER",
       'vdp.model.v1alpha.MgmtPrivateService/LookUpUserAdmin response email': (r) => r && r.message.user.email !== undefined,
-      'vdp.model.v1alpha.MgmtPrivateService/LookUpUserAdmin response plan': (r) => r && r.message.user.plan !== undefined,
-      'vdp.model.v1alpha.MgmtPrivateService/LookUpUserAdmin response billingId': (r) => r && r.message.user.billingId !== undefined,
+      'vdp.model.v1alpha.MgmtPrivateService/LookUpUserAdmin response customerId': (r) => r && r.message.user.customerId !== undefined,
       'vdp.model.v1alpha.MgmtPrivateService/LookUpUserAdmin response firstName': (r) => r && r.message.user.firstName !== undefined,
       'vdp.model.v1alpha.MgmtPrivateService/LookUpUserAdmin response lastName': (r) => r && r.message.user.lastName !== undefined,
       'vdp.model.v1alpha.MgmtPrivateService/LookUpUserAdmin response orgName': (r) => r && r.message.user.orgName !== undefined,
@@ -169,16 +167,13 @@ export function CheckAdminUpdate() {
       name: `users/${constant.defaultUser.id}`,
       type: "OWNER_TYPE_ORGANIZATION",
       email: "test@foo.bar",
-      plan: "plans/new_plan",
-      billing_id: "0",
+      customer_id: "new_customer_id",
       first_name: "test",
       last_name: "foo",
       org_name: "company",
       role: "ai-researcher",
       newsletter_subscription: true,
       cookie_token: "f5730f62-7026-4e11-917a-d890da315d3b",
-      create_time: "2000-01-01T00:00:00.000000Z",
-      update_time: "2000-01-01T00:00:00.000000Z",
     };
 
     var res = client.invoke('vdp.mgmt.v1alpha.MgmtPrivateService/GetUserAdmin', {
@@ -187,7 +182,7 @@ export function CheckAdminUpdate() {
 
     check(client.invoke('vdp.mgmt.v1alpha.MgmtPrivateService/UpdateUserAdmin', {
       user: userUpdate,
-      update_mask: "email,plan,billingId,firstName,lastName,orgName,role,newsletterSubscription,cookieToken,updateTime"
+      update_mask: "email,firstName,lastName,orgName,role,newsletterSubscription,cookieToken"
     }), {
       'vdp.model.v1alpha.MgmtPrivateService/UpdateUserAdmin status': (r) => r && r.status == grpc.StatusOK,
       'vdp.model.v1alpha.MgmtPrivateService/UpdateUserAdmin response name unchanged': (r) => r && r.message.user.name === res.message.user.name,
@@ -195,8 +190,7 @@ export function CheckAdminUpdate() {
       'vdp.model.v1alpha.MgmtPrivateService/UpdateUserAdmin response id unchanged': (r) => r && r.message.user.id === res.message.user.id,
       'vdp.model.v1alpha.MgmtPrivateService/UpdateUserAdmin response type unchanged': (r) => r && r.message.user.type === res.message.user.type,
       'vdp.model.v1alpha.MgmtPrivateService/UpdateUserAdmin response email updated': (r) => r && r.message.user.email === userUpdate.email,
-      'vdp.model.v1alpha.MgmtPrivateService/UpdateUserAdmin response plan updated': (r) => r && r.message.user.plan === userUpdate.plan,
-      'vdp.model.v1alpha.MgmtPrivateService/UpdateUserAdmin response billingId updated': (r) => r && r.message.user.billingId === userUpdate.billing_id,
+      'vdp.model.v1alpha.MgmtPrivateService/UpdateUserAdmin response customerId unchanged': (r) => r && r.message.user.customerId === res.message.user.customerId,
       'vdp.model.v1alpha.MgmtPrivateService/UpdateUserAdmin response firstName updated': (r) => r && r.message.user.firstName === userUpdate.first_name,
       'vdp.model.v1alpha.MgmtPrivateService/UpdateUserAdmin response lastName updated': (r) => r && r.message.user.lastName === userUpdate.last_name,
       'vdp.model.v1alpha.MgmtPrivateService/UpdateUserAdmin response orgName updated': (r) => r && r.message.user.orgName === userUpdate.org_name,
@@ -210,7 +204,7 @@ export function CheckAdminUpdate() {
     // Restore to default user
     check(client.invoke('vdp.mgmt.v1alpha.MgmtPrivateService/UpdateUserAdmin', {
       user: constant.defaultUser,
-      update_mask: "email,plan,billingId,firstName,lastName,orgName,role,newsletterSubscription,cookieToken,updateTime"
+      update_mask: "email,firstName,lastName,orgName,role,newsletterSubscription,cookieToken"
     }), {
       'vdp.model.v1alpha.MgmtPrivateService/UpdateUserAdmin status': (r) => r && r.status == grpc.StatusOK,
     });
