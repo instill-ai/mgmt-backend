086deb5f3e1d5789806f2426ebe7f7e43d1edbac
Xiaofei Du <xiaofei.du@instill.tech>
2023-03-30 16:36:42 +0100

    chore: add constant value

--- pkg/constant/constant.go
+++ pkg/constant/constant.go
@@ -2,3 +2,6 @@ package constant
 
 // HeaderUserUIDKey is the header key for the User UID
 const HeaderUserUIDKey = "Jwt-Sub"
+
+// MaxApiKeyNum is the maximum number of API keys
+const MaxApiKeyNum = 10
