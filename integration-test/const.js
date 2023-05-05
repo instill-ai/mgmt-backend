34589c82934e1e2bdbde26a4a13f8e9e2c89181e
Chang, Hui-Tang <huitang.chang@instill.tech>
2023-05-05 16:23:21 +0800

    chore: refactor integration test mode (#70)
    Because
    
    - Need to simplify the integration test mode
    
    This commit
    
    - Add `API_GATEWAY_HOST`, `API_GATEWAY_PORT` and `API_GATEWAY_PROTOCOL`
    var. to simplify the integration test setting

--- integration-test/const.js
+++ integration-test/const.js
@@ -5,27 +5,28 @@ let host
 let publicPort
 let privatePort
 
-if (__ENV.MODE == "api-gateway") {
-  // api-gateway mode for accessing api-gateway directly
+if (__ENV.API_GATEWAY_HOST && !__ENV.API_GATEWAY_PORT || !__ENV.API_GATEWAY_HOST && __ENV.API_GATEWAY_PORT) {
+  fail("both API_GATEWAY_HOST and API_GATEWAY_PORT should be properly configured.")
+}
+
+export const apiGatewayMode = (__ENV.API_GATEWAY_HOST && __ENV.API_GATEWAY_PORT);
+
+if (__ENV.API_GATEWAY_PROTOCOL) {
+  if (__ENV.API_GATEWAY_PROTOCOL !== "http" && __ENV.API_GATEWAY_PROTOCOL != "https") {
+    fail("only allow `http` or `https` for API_GATEWAY_PROTOCOL")
+  }
+  proto = __ENV.API_GATEWAY_PROTOCOL
+} else {
   proto = "http"
-  host = "api-gateway"
+}
+
+if (apiGatewayMode) {
+  // api gateway mode
+  host = __ENV.API_GATEWAY_HOST
   privatePort = 3084
-  publicPort = 8080
-} else if (__ENV.MODE == "localhost") {
-  // localhost mode for accessing api-gateway from localhost
-  proto = "http"
-  host = "localhost"
-  privatePort = 3084
-  publicPort = 8080
-} else if (__ENV.MODE == "internal") {
-  // internal mode for accessing api-gateway from container
-  proto = "http"
-  host = "host.docker.internal"
-  privatePort = 3084
-  publicPort = 8080
+  publicPort = __ENV.API_GATEWAY_PORT
 } else {
   // direct microservice mode
-  proto = "http"
   host = "mgmt-backend"
   privatePort = 3084
   publicPort = 8084
