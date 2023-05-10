81e4d937e9b5f9784686d8afe4489a6aec0c5ded
Xiaofei Du <xiaofei.du@instill.tech>
2023-05-10 18:47:06 +0100

    chore: fix usage edition initialisation (#74)
    Because
    
    - the edition is not initialised in usage
    
    This commit
    
    - initialise edition in usage
    
    ---------
    
    Co-authored-by: Chang, Hui-Tang <huitang.chang@instill.tech>

--- pkg/middleware/misc.go
+++ pkg/middleware/misc.go
@@ -16,6 +16,7 @@ import (
 	"google.golang.org/grpc/status"
 	"google.golang.org/protobuf/proto"
 
+	"github.com/instill-ai/mgmt-backend/config"
 	"github.com/instill-ai/mgmt-backend/pkg/constant"
 	"github.com/instill-ai/mgmt-backend/pkg/logger"
 )
@@ -70,7 +71,7 @@ func HttpResponseModifier(ctx context.Context, w http.ResponseWriter, p proto.Me
 
 // ErrorHandler is a callback function for gRPC-Gateway runtime.WithErrorHandler
 func ErrorHandler(ctx context.Context, mux *runtime.ServeMux, marshaler runtime.Marshaler, w http.ResponseWriter, r *http.Request, err error) {
-	logger, _ := logger.GetZapLogger()
+	logger, _ := logger.GetZapLogger(config.Config.Server.Debug)
 
 	// return Internal when Marshal failed
 	const fallback = `{"code": 13, "message": "failed to marshal error message"}`
