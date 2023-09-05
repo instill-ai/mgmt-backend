96c92ff31a2d3fcc073668da27dc27d8e1253ee2
Heiru Wu <heiru.wu@instill.tech>
2023-09-06 03:21:15 +0800

    chore: tidy up lookup function

--- pkg/service/metric.go
+++ pkg/service/metric.go
@@ -14,7 +14,7 @@ import (
 	pipelinePB "github.com/instill-ai/protogen-go/vdp/pipeline/v1alpha"
 )
 
-func (s *service) ListPipelineTriggerRecords(ctx context.Context, owner *mgmtPB.User, pageSize int64, pageToken string, filter filtering.Filter) ([]*mgmtPB.PipelineTriggerRecord, int64, string, error) {
+func (s *service) pipelineUIDLookup(ctx context.Context, filter filtering.Filter, owner *mgmtPB.User) filtering.Filter {
 
 	// lookup pipeline uid
 	if len(filter.CheckedExpr.GetExpr().GetCallExpr().GetArgs()) > 0 {
@@ -37,6 +37,30 @@ func (s *service) ListPipelineTriggerRecords(ctx context.Context, owner *mgmtPB.
 			}
 		}
 	}
+
+	return filter
+}
+
+func (s *service) connectorUIDLookup(ctx context.Context, filter filtering.Filter, owner *mgmtPB.User) filtering.Filter {
+
+	// lookup connector uid
+	if len(filter.CheckedExpr.GetExpr().GetCallExpr().GetArgs()) > 0 {
+		connectorID, _ := repository.ExtractConstExpr(filter.CheckedExpr.GetExpr(), constant.ConnectorID, false)
+
+		respConnector, err := s.connectorPublicServiceClient.GetUserConnectorResource(ctx, &connectorPB.GetUserConnectorResourceRequest{
+			Name: fmt.Sprintf("%s/connectors/%s", owner.Name, connectorID),
+		})
+		if err == nil {
+			repository.HijackConstExpr(filter.CheckedExpr.GetExpr(), constant.ConnectorID, constant.ConnectorUID, respConnector.ConnectorResource.Uid, false)
+		}
+	}
+
+	return filter
+}
+
+func (s *service) ListPipelineTriggerRecords(ctx context.Context, owner *mgmtPB.User, pageSize int64, pageToken string, filter filtering.Filter) ([]*mgmtPB.PipelineTriggerRecord, int64, string, error) {
+
+	filter = s.pipelineUIDLookup(ctx, filter, owner)
 
 	pipelineTriggerRecords, ps, pt, err := s.influxDB.QueryPipelineTriggerRecords(ctx, *owner.Uid, pageSize, pageToken, filter)
 	if err != nil {
@@ -48,27 +72,7 @@ func (s *service) ListPipelineTriggerRecords(ctx context.Context, owner *mgmtPB.
 
 func (s *service) ListPipelineTriggerTableRecords(ctx context.Context, owner *mgmtPB.User, pageSize int64, pageToken string, filter filtering.Filter) ([]*mgmtPB.PipelineTriggerTableRecord, int64, string, error) {
 
-	// lookup pipeline uid
-	if len(filter.CheckedExpr.GetExpr().GetCallExpr().GetArgs()) > 0 {
-		pipelineID, _ := repository.ExtractConstExpr(filter.CheckedExpr.GetExpr(), constant.PipelineID, false)
-
-		respPipeline, err := s.pipelinePublicServiceClient.GetUserPipeline(ctx, &pipelinePB.GetUserPipelineRequest{
-			Name: fmt.Sprintf("%s/pipelines/%s", owner.Name, pipelineID),
-		})
-		if err == nil {
-			repository.HijackConstExpr(filter.CheckedExpr.GetExpr(), constant.PipelineID, constant.PipelineUID, respPipeline.Pipeline.Uid, false)
-
-			// lookup pipeline release uid
-			pipelineReleaseID, _ := repository.ExtractConstExpr(filter.CheckedExpr.GetExpr(), constant.PipelineReleaseID, false)
-
-			respPipelineRelease, err := s.pipelinePublicServiceClient.GetUserPipelineRelease(ctx, &pipelinePB.GetUserPipelineReleaseRequest{
-				Name: fmt.Sprintf("%s/pipelines/%s/releases/%s", owner.Name, pipelineID, pipelineReleaseID),
-			})
-			if err == nil {
-				repository.HijackConstExpr(filter.CheckedExpr.GetExpr(), constant.PipelineID, constant.PipelineUID, respPipelineRelease.Release.Uid, false)
-			}
-		}
-	}
+	filter = s.pipelineUIDLookup(ctx, filter, owner)
 
 	pipelineTriggerTableRecords, ps, pt, err := s.influxDB.QueryPipelineTriggerTableRecords(ctx, *owner.Uid, pageSize, pageToken, filter)
 	if err != nil {
@@ -80,27 +84,7 @@ func (s *service) ListPipelineTriggerTableRecords(ctx context.Context, owner *mg
 
 func (s *service) ListPipelineTriggerChartRecords(ctx context.Context, owner *mgmtPB.User, aggregationWindow int64, filter filtering.Filter) ([]*mgmtPB.PipelineTriggerChartRecord, error) {
 
-	// lookup pipeline uid
-	if len(filter.CheckedExpr.GetExpr().GetCallExpr().GetArgs()) > 0 {
-		pipelineID, _ := repository.ExtractConstExpr(filter.CheckedExpr.GetExpr(), constant.PipelineID, false)
-
-		respPipeline, err := s.pipelinePublicServiceClient.GetUserPipeline(ctx, &pipelinePB.GetUserPipelineRequest{
-			Name: fmt.Sprintf("%s/pipelines/%s", owner.Name, pipelineID),
-		})
-		if err == nil {
-			repository.HijackConstExpr(filter.CheckedExpr.GetExpr(), constant.PipelineID, constant.PipelineUID, respPipeline.Pipeline.Uid, false)
-
-			// lookup pipeline release uid
-			pipelineReleaseID, _ := repository.ExtractConstExpr(filter.CheckedExpr.GetExpr(), constant.PipelineReleaseID, false)
-
-			respPipelineRelease, err := s.pipelinePublicServiceClient.GetUserPipelineRelease(ctx, &pipelinePB.GetUserPipelineReleaseRequest{
-				Name: fmt.Sprintf("%s/pipelines/%s/releases/%s", owner.Name, pipelineID, pipelineReleaseID),
-			})
-			if err == nil {
-				repository.HijackConstExpr(filter.CheckedExpr.GetExpr(), constant.PipelineID, constant.PipelineUID, respPipelineRelease.Release.Uid, false)
-			}
-		}
-	}
+	filter = s.pipelineUIDLookup(ctx, filter, owner)
 
 	pipelineTriggerChartRecords, err := s.influxDB.QueryPipelineTriggerChartRecords(ctx, *owner.Uid, aggregationWindow, filter)
 	if err != nil {
@@ -112,36 +96,9 @@ func (s *service) ListPipelineTriggerChartRecords(ctx context.Context, owner *mg
 
 func (s *service) ListConnectorExecuteRecords(ctx context.Context, owner *mgmtPB.User, pageSize int64, pageToken string, filter filtering.Filter) ([]*mgmtPB.ConnectorExecuteRecord, int64, string, error) {
 
-	if len(filter.CheckedExpr.GetExpr().GetCallExpr().GetArgs()) > 0 {
-		pipelineID, _ := repository.ExtractConstExpr(filter.CheckedExpr.GetExpr(), constant.PipelineID, false)
+	filter = s.pipelineUIDLookup(ctx, filter, owner)
 
-		respPipeline, err := s.pipelinePublicServiceClient.GetUserPipeline(ctx, &pipelinePB.GetUserPipelineRequest{
-			Name: fmt.Sprintf("%s/pipelines/%s", owner.Name, pipelineID),
-		})
-		if err == nil {
-			repository.HijackConstExpr(filter.CheckedExpr.GetExpr(), constant.PipelineID, constant.PipelineUID, respPipeline.Pipeline.Uid, false)
-
-			// lookup pipeline release uid
-			pipelineReleaseID, _ := repository.ExtractConstExpr(filter.CheckedExpr.GetExpr(), constant.PipelineReleaseID, false)
-
-			respPipelineRelease, err := s.pipelinePublicServiceClient.GetUserPipelineRelease(ctx, &pipelinePB.GetUserPipelineReleaseRequest{
-				Name: fmt.Sprintf("%s/pipelines/%s/releases/%s", owner.Name, pipelineID, pipelineReleaseID),
-			})
-			if err == nil {
-				repository.HijackConstExpr(filter.CheckedExpr.GetExpr(), constant.PipelineID, constant.PipelineUID, respPipelineRelease.Release.Uid, false)
-			}
-		}
-
-		// lookup connector uid
-		connectorID, _ := repository.ExtractConstExpr(filter.CheckedExpr.GetExpr(), constant.ConnectorID, false)
-
-		respConnector, err := s.connectorPublicServiceClient.GetUserConnectorResource(ctx, &connectorPB.GetUserConnectorResourceRequest{
-			Name: fmt.Sprintf("%s/connectors/%s", owner.Name, connectorID),
-		})
-		if err == nil {
-			repository.HijackConstExpr(filter.CheckedExpr.GetExpr(), constant.ConnectorID, constant.ConnectorUID, respConnector.ConnectorResource.Uid, false)
-		}
-	}
+	filter = s.connectorUIDLookup(ctx, filter, owner)
 
 	connectorExecuteRecords, ps, pt, err := s.influxDB.QueryConnectorExecuteRecords(ctx, *owner.Uid, pageSize, pageToken, filter)
 	if err != nil {
@@ -153,17 +110,7 @@ func (s *service) ListConnectorExecuteRecords(ctx context.Context, owner *mgmtPB
 
 func (s *service) ListConnectorExecuteTableRecords(ctx context.Context, owner *mgmtPB.User, pageSize int64, pageToken string, filter filtering.Filter) ([]*mgmtPB.ConnectorExecuteTableRecord, int64, string, error) {
 
-	if len(filter.CheckedExpr.GetExpr().GetCallExpr().GetArgs()) > 0 {
-		// lookup connector uid
-		connectorID, _ := repository.ExtractConstExpr(filter.CheckedExpr.GetExpr(), constant.ConnectorID, false)
-
-		respConnector, err := s.connectorPublicServiceClient.GetUserConnectorResource(ctx, &connectorPB.GetUserConnectorResourceRequest{
-			Name: fmt.Sprintf("%s/connectors/%s", owner.Name, connectorID),
-		})
-		if err == nil {
-			repository.HijackConstExpr(filter.CheckedExpr.GetExpr(), constant.ConnectorID, constant.ConnectorUID, respConnector.ConnectorResource.Uid, false)
-		}
-	}
+	filter = s.connectorUIDLookup(ctx, filter, owner)
 
 	connectorExecuteTableRecords, ps, pt, err := s.influxDB.QueryConnectorExecuteTableRecords(ctx, *owner.Uid, pageSize, pageToken, filter)
 	if err != nil {
@@ -175,36 +122,9 @@ func (s *service) ListConnectorExecuteTableRecords(ctx context.Context, owner *m
 
 func (s *service) ListConnectorExecuteChartRecords(ctx context.Context, owner *mgmtPB.User, aggregationWindow int64, filter filtering.Filter) ([]*mgmtPB.ConnectorExecuteChartRecord, error) {
 
-	if len(filter.CheckedExpr.GetExpr().GetCallExpr().GetArgs()) > 0 {
-		pipelineID, _ := repository.ExtractConstExpr(filter.CheckedExpr.GetExpr(), constant.PipelineID, false)
+	filter = s.pipelineUIDLookup(ctx, filter, owner)
 
-		respPipeline, err := s.pipelinePublicServiceClient.GetUserPipeline(ctx, &pipelinePB.GetUserPipelineRequest{
-			Name: fmt.Sprintf("%s/pipelines/%s", owner.Name, pipelineID),
-		})
-		if err == nil {
-			repository.HijackConstExpr(filter.CheckedExpr.GetExpr(), constant.PipelineID, constant.PipelineUID, respPipeline.Pipeline.Uid, false)
-
-			// lookup pipeline release uid
-			pipelineReleaseID, _ := repository.ExtractConstExpr(filter.CheckedExpr.GetExpr(), constant.PipelineReleaseID, false)
-
-			respPipelineRelease, err := s.pipelinePublicServiceClient.GetUserPipelineRelease(ctx, &pipelinePB.GetUserPipelineReleaseRequest{
-				Name: fmt.Sprintf("%s/pipelines/%s/releases/%s", owner.Name, pipelineID, pipelineReleaseID),
-			})
-			if err == nil {
-				repository.HijackConstExpr(filter.CheckedExpr.GetExpr(), constant.PipelineID, constant.PipelineUID, respPipelineRelease.Release.Uid, false)
-			}
-		}
-
-		// lookup connector uid
-		connectorID, _ := repository.ExtractConstExpr(filter.CheckedExpr.GetExpr(), constant.ConnectorID, false)
-
-		respConnector, err := s.connectorPublicServiceClient.GetUserConnectorResource(ctx, &connectorPB.GetUserConnectorResourceRequest{
-			Name: fmt.Sprintf("%s/connectors/%s", owner.Name, connectorID),
-		})
-		if err == nil {
-			repository.HijackConstExpr(filter.CheckedExpr.GetExpr(), constant.ConnectorID, constant.ConnectorUID, respConnector.ConnectorResource.Uid, false)
-		}
-	}
+	filter = s.connectorUIDLookup(ctx, filter, owner)
 
 	connectorExecuteChartRecords, err := s.influxDB.QueryConnectorExecuteChartRecords(ctx, *owner.Uid, aggregationWindow, filter)
 	if err != nil {
