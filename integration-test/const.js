6cce71b49716bf95bc8cc821cb3d267527e89254
Chang, Hui-Tang <huitang.chang@instill.tech>
2023-09-12 19:48:22 +0800

    chore: update integration test to support consolidated gateway (#119)
        Because
    
        - we consolidated three gateways into one.
    
        This commit
    
        - update integration test to support consolidated gateway

--- integration-test/const.js
+++ integration-test/const.js
@@ -1,15 +1,8 @@
 import { uuidv4 } from 'https://jslib.k6.io/k6-utils/1.4.0/index.js';
 
 let proto
-let host
-let publicPort
-let privatePort
 
-if (__ENV.API_GATEWAY_BASE_HOST && !__ENV.API_GATEWAY_BASE_PORT || !__ENV.API_GATEWAY_BASE_HOST && __ENV.API_GATEWAY_BASE_PORT) {
-  fail("both API_GATEWAY_BASE_HOST and API_GATEWAY_BASE_PORT should be properly configured.")
-}
-
-export const apiGatewayMode = (__ENV.API_GATEWAY_BASE_HOST && __ENV.API_GATEWAY_BASE_PORT);
+export const apiGatewayMode = (__ENV.API_GATEWAY_URL !== "" && __ENV.API_GATEWAY_URL !== undefined);
 
 if (__ENV.API_GATEWAY_PROTOCOL) {
   if (__ENV.API_GATEWAY_PROTOCOL !== "http" && __ENV.API_GATEWAY_PROTOCOL != "https") {
@@ -19,25 +12,12 @@ if (__ENV.API_GATEWAY_PROTOCOL) {
 } else {
   proto = "http"
 }
-
-if (apiGatewayMode) {
-  // api gateway mode
-  host = __ENV.API_GATEWAY_BASE_HOST
-  privatePort = 3084
-  publicPort = __ENV.API_GATEWAY_BASE_PORT
-} else {
-  // direct microservice mode
-  host = "mgmt-backend"
-  privatePort = 3084
-  publicPort = 8084
-}
-
 export const mgmtVersion = "v1alpha";
-export const mgmtPrivateHost = `${proto}://${host}:${privatePort}/${mgmtVersion}`
-export const mgmtPublicHost = `${proto}://${host}:${publicPort}/${mgmtVersion}`
+export const mgmtPrivateHost = apiGatewayMode ? "" : `http://mgmt-backend:3084/${mgmtVersion}`
+export const mgmtPublicHost = apiGatewayMode ? `${proto}://${__ENV.API_GATEWAY_URL}/base/${mgmtVersion}` : `http://mgmt-backend:8084/${mgmtVersion}`
 
-export const mgmtPrivateGRPCHost = `${host}:${privatePort}`
-export const mgmtPublicGRPCHost = `${host}:${publicPort}`
+export const mgmtPrivateGRPCHost = `mgmt-backend:3084`
+export const mgmtPublicGRPCHost = `${__ENV.API_GATEWAY_URL}`
 
 export const restParams = {
   headers: {
