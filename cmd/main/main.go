806ccd7211724cc6e67197fda6c00f89fbae4102
Heiru Wu <heiru.wu@instill.tech>
2023-10-11 22:52:42 +0800

    chore: fix regex

--- cmd/main/main.go
+++ cmd/main/main.go
@@ -100,14 +100,11 @@ func main() {
 		grpc_zap.WithDecider(func(fullMethodName string, err error) bool {
 			// will not log gRPC calls if it was a call to liveness or readiness and no error was raised
 			if err == nil {
-				if match, _ := regexp.MatchString("base.mgmt.v1alpha.MgmtPrivateService/.*ness$", fullMethodName); match {
+				// stop logging sucessful private function calls
+				if match, _ := regexp.MatchString("base.mgmt.v1alpha.MgmtPrivateService/.*(?:Admin|ness)$", fullMethodName); match {
 					return false
 				}
 				if match, _ := regexp.MatchString("base.mgmt.v1alpha.MgmtPublicService/.*ness$", fullMethodName); match {
-					return false
-				}
-				// stop logging successful private function calls
-				if match, _ := regexp.MatchString("model.model.v1alpha.ModelPrivateService/.*Admin$", fullMethodName); match {
 					return false
 				}
 			}
