afdcd5f656503d08734fc14cd965c31b4326d259
Chang, Hui-Tang <huitang.chang@instill.tech>
2024-03-22 02:44:34 -0500

    feat: add configuration for read-replica database (#189)
    Because
    
    - We are going to support a read-replica database to improve query
    throughput.
    
    This commit
    
    - Adds configuration for the read-replica database.
    
    Note
    
    - When using a read-replica database, we need to consider the
    read-after-write inconsistency problem. This issue will be addressed in
    the upcoming PR.

--- pkg/db/db.go
+++ pkg/db/db.go
@@ -7,6 +7,7 @@ import (
 	"gorm.io/driver/postgres"
 	"gorm.io/gorm"
 	"gorm.io/gorm/schema"
+	"gorm.io/plugin/dbresolver"
 
 	config "github.com/instill-ai/mgmt-backend/config"
 )
@@ -38,6 +39,24 @@ func GetConnection(databaseConfig *config.DatabaseConfig) *gorm.DB {
 
 		if err != nil {
 			panic("Could not open database connection")
+		}
+
+		if databaseConfig.Replica.Host != "" {
+			replicaDSN := fmt.Sprintf("host=%s user=%s password=%s dbname=%s port=%d sslmode=disable TimeZone=%s",
+				databaseConfig.Replica.Host,
+				databaseConfig.Replica.Username,
+				databaseConfig.Replica.Password,
+				databaseConfig.Name,
+				databaseConfig.Replica.Port,
+				databaseConfig.TimeZone,
+			)
+			err = db.Use(dbresolver.Register(dbresolver.Config{
+				Replicas:          []gorm.Dialector{postgres.Open(replicaDSN)},
+				TraceResolverMode: true,
+			}))
+			if err != nil {
+				panic("Could not open replica database connection")
+			}
 		}
 
 		sqlDB, _ := db.DB()
