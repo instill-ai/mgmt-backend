c195abf985f50822078e3658c03a9888cca9002d
HR Wu <5631010+heiruwu@users.noreply.github.com>
2024-04-30 00:00:44 +0800

    chore(metric): fix wrongly used grpc return code (#205)
    Because
    
    - If the requests for metric related endpoints have a wrong `owner_name`
    namespace format in filter that is not adhering to
    `{user_type}/{user_id}`, should return `400` http status code
    
    This commit
    
    - return `InvalidArgument` if the namespace format is wrong for
    `owner_name` filter

--- pkg/service/metric.go
+++ pkg/service/metric.go
@@ -61,7 +61,7 @@ func (s *service) checkPipelineOwnership(ctx context.Context, filter filtering.F
 				repository.HijackConstExpr(filter.CheckedExpr.GetExpr(), constant.OwnerName, constant.PipelineOwnerUID, org.Uid, false)
 				ownerUID = &org.Uid
 			} else {
-				return nil, "", "", "", filter, fmt.Errorf("owner_name namepsace format error")
+				return nil, "", "", "", filter, ErrInvalidOwnerNamespace
 			}
 		} else {
 			ownerQueryString = fmt.Sprintf("|> filter(fn: (r) => r[\"owner_uid\"] == \"%v\")", *owner.Uid)
